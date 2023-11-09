28ef348c45dee0c1d2183af7aa826897394729ba
CDNunez <carlosnunez114@gmail.com>
2023-11-08 20:50:17 -0500

    Finished project

--- index.js
+++ index.js
@@ -81,14 +81,20 @@ class Store {
         this.balance = Number (storeBalance.toFixed(2));//updates balance
         let storeExpense = this.expense + item.purchasePrice;
         this.expense = Number (storeExpense.toFixed(2))//updates expense
-        if (this.inventory.includes(item) && item.upc === item.upc) {//checks if item is already in inventory
-             item.quantity = item.quantity + item.quantity;//updates the quantity of item in stock
+        
+        let itemFilter = this.inventory.find(box => { //creates variable that returns whether the variable's upc matches the object's upc
+            return box.upc === item.upc
+        });
+            
+        if (itemFilter) {  //if the upc from itemFilter matches the upc from the item passed through then item.quantity is updated
+            itemFilter.quantity += item.quantity;//if the item exists, the quantity increases
         } else {
-            this.inventory.push(item);//else stocks item
+            this.inventory.push(item);//if the item doesn't exist in the array, it is pushed through
         }
-        markUp =item.purchasePrice + (item.purchasePrice * markUp);
-        let taxPrice = markUp * this.tax;
-        let wholeSale = markUp + taxPrice;
+
+        markUp =item.purchasePrice + (item.purchasePrice * markUp); //defines the markUp price of the item
+        let taxPrice = markUp * this.tax;//defines the tax amount of the item
+        let wholeSale = markUp + taxPrice;//defines the wholeSale price of the item
         item.marketPrice =Number (wholeSale.toFixed(2)); //updates item market price
         console.log(`${this.name} stocked ${item.quantity} ${item.itemName}.`) //this is just to keep track when method fires
     } else if(this.balance < item.purchasePrice) {//conditional to log statement if the store does not have enough funds
@@ -100,20 +106,21 @@ class Store {
     }
 
     sellItem(item, toSell){//class method to sell item in store class
-        if (item.quantity >= 1) {//conditional to confirm item is in stock
+        if (item.quantity >= 1 && item.quantity >= toSell) {//conditional to confirm item is in stock and if there is enough to sell it
             let storeBalance = this.balance + item.marketPrice;
             this.balance = Number (storeBalance.toFixed(2)); //update store balance
             let storeProfit = this.profit + (item.marketPrice - item.purchasePrice);
             this.profit = Number (storeProfit.toFixed(2)); //update store profit
-            item.quantity = item.quantity - toSell;
+            item.quantity = item.quantity - toSell;//updates item quantity by subtracting how many items toSell
             let taxWithheld = this.paidTax + (item.marketPrice * this.tax);
             this.paidTax  = Number (taxWithheld.toFixed(2)); //update paidTax
             console.log(`${this.name} sold ${toSell} ${item.itemName}.`) //keeps track of method
         } else {
             console.log(`${this.name} out of ${item.itemName}.`);
+        }
     }
 }
-}
+
 
 
 //*Product Class
@@ -138,21 +145,21 @@ class Product { //class to create objects with product parameters
 
 //?MA Stores
 const maStore1 = Store.addStore("Big Al's Thrift Store", {state:"MA" , city: "Springfield"}, 21);
-const maStore2 = Store.addStore("Big Beth's Thrift Store", {state:"MA" , city: "Holyoke"}, 21);
-const maStore3 = Store.addStore("Big Carl's Thrift Store", {state:"MA" , city: "Westfield"}, 21);
+//const maStore2 = Store.addStore("Big Beth's Thrift Store", {state:"MA" , city: "Holyoke"}, 21);
+//const maStore3 = Store.addStore("Big Carl's Thrift Store", {state:"MA" , city: "Westfield"}, 21);
 
 // console.log(maStore1,maStore2,maStore3);
 
 //? CT Stores
-const ctStore1 = Store.addStore("Medium Dave's Thrift Store", {state: "CT", city: "Windsor"}, 6);
+//const ctStore1 = Store.addStore("Medium Dave's Thrift Store", {state: "CT", city: "Windsor"}, 6);
 const ctStore2 = Store.addStore("Medium Ellis' Thrift Store", {state: "CT", city: "Waterbury"}, 6);
-const ctStore3 = Store.addStore("Medium Fabian's Thrift Store", {state: "CT", city: "Hartford"}, 6);
+//const ctStore3 = Store.addStore("Medium Fabian's Thrift Store", {state: "CT", city: "Hartford"}, 6);
 
 // console.log(ctStore1,ctStore2,ctStore3);
 
 //?MN Stores
-const mnStore1 = Store.addStore("Small Giovanni's Thrift Store", {state: "MN", city:"placeholder"}, 19);
-const mnStore2 = Store.addStore("Small Helena's Thrift Store", {state: "MN", city:"placeholder"}, 19);
+//const mnStore1 = Store.addStore("Small Giovanni's Thrift Store", {state: "MN", city:"placeholder"}, 19);
+//const mnStore2 = Store.addStore("Small Helena's Thrift Store", {state: "MN", city:"placeholder"}, 19);
 const mnStore3 = Store.addStore("Small Ismael's Thrift Store", {state: "MN", city:"Portland"}, 19);
 
 // console.log(mnStore1,mnStore2,mnStore3);
@@ -161,6 +168,7 @@ const mnStore3 = Store.addStore("Small Ismael's Thrift Store", {state: "MN", cit
 
 //! Inventory
 const pins = new Product(10, 'Pins', 'Accessories', 1, 10);
+const animalPins = new Product(10, 'Pins', 'Accessories', 1, 10);
 const badges = new Product(10, 'Badges', 'Accessories', 1, 5);
 
 const lipStick = new Product(11, "Lip Stick", 'Makeup', 5, 1);
@@ -187,6 +195,7 @@ maStore1.addItem(shirts, .6);
 //* Second Store
 ctStore2.addItem(pins, .5);
 ctStore2.addItem(pins, .5);
+ctStore2.addItem(animalPins, .5);
 ctStore2.addItem(badges, .8);
 //* Third Store
 mnStore3.addItem(shoes, .5);
@@ -201,6 +210,7 @@ maStore1.sellItem(bootCutJeans, 1);
 ctStore2.sellItem(pins, 5);
 //* Third Store
 mnStore3.sellItem(belt, 10);
+mnStore3.sellItem(belt, 10);//test to try out overselling an item
 //! Testing
 /* 
     Simply console log each store to check the completed details.
